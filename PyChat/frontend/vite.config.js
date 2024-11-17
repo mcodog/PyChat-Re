545050aaf6c84fc2ff1909b48dd141b5127ba83b
mcodog <156042950+mcodog@users.noreply.github.com>
2024-11-18 04:36:19 +0800

    Update vite.config.js

--- PyChat/frontend/vite.config.js
+++ PyChat/frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // or a specific IP like '127.0.0.1'
+    port: 3000 // Replace with your desired port
+  }
 })

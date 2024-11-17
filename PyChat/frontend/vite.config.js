fb919e73011370880af521c533412fb7bb5bc766
mcodog <156042950+mcodog@users.noreply.github.com>
2024-11-18 04:13:31 +0800

    Update vite.config.js

--- PyChat/frontend/vite.config.js
+++ PyChat/frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true, // Exposes the app to external networks
+    port: process.env.PORT || 3000,
 })
